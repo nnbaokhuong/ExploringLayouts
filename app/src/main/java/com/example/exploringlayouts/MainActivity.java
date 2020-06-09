f2cd5afd57e94df816df177eeac575200a7e0429
Khương Nguyễn Ngọc Bảo <khuong.nguyen@ucdenver.edu>
2020-06-09 17:41:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/exploringlayouts/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.exploringlayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+    void loadConstraintLayout(View v){
+        setContentView(R.layout.activity_main);
+    }
+    void loadTableLayout(View v){
+  //      setContentView(R.layout.my_table_layout);
+    }
+}
